bd18c5d3950a0676852d38433069fc26c1be699f
asadkhalid566 <76807556+asadkhalid566@users.noreply.github.com>
2024-06-04 01:49:24 +0500

    1st

---
+++ client/src/Header.js
@@ -0,0 +1,55 @@
+import { Link } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { UserContext } from "./UserContext";
+
+export default function Header() {
+  const { setUserInfo, userInfo } = useContext(UserContext);
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(userInfo => {
+        setUserInfo(userInfo);
+      })
+      .catch(error => {
+        console.error('Error fetching profile:', error);
+      });
+  }, []);
+  
+
+  function logout() {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
+  return (
+    <header>
+      <Link to="/" className="logo">MyBlog</Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <a onClick={logout}>Logout ({username})</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
